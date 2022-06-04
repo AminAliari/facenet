07cbf081ce299c91f2f3d612d688cee479c0569c
Amin Aliari <mr.amin47@yahoo.com>
2022-06-04 11:19:37 -0400

    Update test_dataset.py

--- tests/unittests/test_dataset.py
+++ tests/unittests/test_dataset.py
@@ -1,10 +1,10 @@
 def test_dataset():
     from facenet.dataio.dataset import FaceDataset
 
-    # dataset = FaceDataset(root_dir='data/samples', img_dim=(256, 256))
-    # loader = dataset.get_data_loader(batch_size=1, use_shuffle=False)
-    # assert(128 == len(loader))
+    dataset = FaceDataset(root_dir='data/samples', img_dim=(256, 256))
+    loader = dataset.get_data_loader(batch_size=1, use_shuffle=False)
+    assert(0 == len(loader))
     # assert((1, 3, 256, 256) == next(iter(loader))[0].shape)
 
-    # note: redacted the data from the public repo. 
+    # note: redacted the data from the public repo.
     pass
